5c8dd215daa3916f9e0f768c66e73559d5a0b3c7
Frazer McLean <frazer@frazermclean.co.uk>
2015-10-02 00:51:09 +0200

    CelestialBody not dependent on ReferenceEllipsoid for mu

--- astrodynamics/bodies/celestialbody.py
+++ astrodynamics/bodies/celestialbody.py
@@ -19,19 +19,37 @@ class CelestialBody(ReprHelperMixin, object):
     Parameters:
         ellipsoid: Representative ellipsoid.
         mass: Mass of the body [kg].
+        mu: Standard gravitational parameter [m\ :sup:`3`\ ·s\ :sup:`-2`]
 
     :type ellipsoid: :py:class:`~astrodynamics.bodies.ellipsoid.Ellipsoid`
     """
-    def __init__(self, ellipsoid, mass):
+    def __init__(self, ellipsoid, mass, mu):
         self._ellipsoid = ellipsoid
         self._mass = verify_unit(mass, 'kg')
+        self._mu = verify_unit(mu, 'm3 / s2')
+
+    @classmethod
+    def from_reference_ellipsoid(cls, ellipsoid, mass):
+        """Construct from a
+        :py:class:`~astrodynamics.bodies.ellipsoid.ReferenceEllipsoid`, which
+        supplies ``mu``.
+
+        Parameters:
+            ellipsoid: Representative ellipsoid.
+            mass: Mass of the body [kg].
+
+        :type ellipsoid: :py:class:`~astrodynamics.bodies.ellipsoid.ReferenceEllipsoid`
+        """
+        return cls(ellipsoid=ellipsoid, mass=mass, mu=ellipsoid.mu)
 
     ellipsoid = read_only_property('_ellipsoid')
     mass = read_only_property('_mass')
+    mu = read_only_property('_mu')
 
     def _repr_helper_(self, r):
         r.keyword_from_attr('ellipsoid')
         # View as Quantity to prevent full Constant repr.
         r.keyword_with_value('mass', self.mass.view(Quantity))
+        r.keyword_with_value('mu', self.mu.view(Quantity))
 
-earth = CelestialBody(ellipsoid=wgs84, mass=EARTH_MASS)
+earth = CelestialBody.from_reference_ellipsoid(ellipsoid=wgs84, mass=EARTH_MASS)
