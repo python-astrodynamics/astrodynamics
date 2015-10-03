16002b001a120410e4f993ad6fb93b123de183cb
Frazer McLean <frazer@frazermclean.co.uk>
2015-10-03 16:06:02 +0200

    Test string form of verify_unit

--- astrodynamics/tests/test_util.py
+++ astrodynamics/tests/test_util.py
@@ -10,10 +10,14 @@ from astrodynamics.util import verify_unit
 def test_verify_unit():
     # Implicit dimensionless values are allowed, test that Quantity is returned.
     assert verify_unit(0, u.one) == 0 * u.one
+    assert verify_unit(0, '') == 0 * u.one
 
     # Test failure mode
     with pytest.raises(ValueError):
         verify_unit(0, u.meter)
+    with pytest.raises(ValueError):
+        verify_unit(0, 'm')
 
     # Quantity should be passed back if unit matches
     assert verify_unit(1 * u.meter, u.meter) == 1 * u.meter
+    assert verify_unit(1 * u.meter, 'm') == 1 * u.meter
