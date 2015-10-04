11480f780740185def3de5429aed58982d664eda
Frazer McLean <frazer@frazermclean.co.uk>
2015-10-04 23:14:12 +0200

    Add assertion for checks, remove untested platforms

--- astrodynamics/tests/test_coverage.py
+++ astrodynamics/tests/test_coverage.py
@@ -12,53 +12,33 @@ def test_platform_coverage():
 
     machine = (os, python_version, is64bit)
 
+    assert os in ('Darwin', 'Linux', 'Windows')
+    assert python_version in [
+        (2, 7),
+        (3, 3),
+        (3, 4),
+        (3, 5),
+    ]
+
     if machine == ('Darwin', (2, 7), True):
-        pass
-    elif machine == ('Darwin', (2, 7), False):
-        pass
-    elif machine == ('Darwin', (3, 2), True):
-        pass
-    elif machine == ('Darwin', (3, 2), False):
         pass
     elif machine == ('Darwin', (3, 3), True):
         pass
-    elif machine == ('Darwin', (3, 3), False):
-        pass
     elif machine == ('Darwin', (3, 4), True):
-        pass
-    elif machine == ('Darwin', (3, 4), False):
         pass
     elif machine == ('Darwin', (3, 5), True):
         pass
-    elif machine == ('Darwin', (3, 5), False):
-        pass
     elif machine == ('Linux', (2, 7), True):
-        pass
-    elif machine == ('Linux', (2, 7), False):
-        pass
-    elif machine == ('Linux', (3, 2), True):
-        pass
-    elif machine == ('Linux', (3, 2), False):
         pass
     elif machine == ('Linux', (3, 3), True):
         pass
-    elif machine == ('Linux', (3, 3), False):
-        pass
     elif machine == ('Linux', (3, 4), True):
         pass
-    elif machine == ('Linux', (3, 4), False):
-        pass
     elif machine == ('Linux', (3, 5), True):
-        pass
-    elif machine == ('Linux', (3, 5), False):
         pass
     elif machine == ('Windows', (2, 7), True):
         pass
     elif machine == ('Windows', (2, 7), False):
-        pass
-    elif machine == ('Windows', (3, 2), True):
-        pass
-    elif machine == ('Windows', (3, 2), False):
         pass
     elif machine == ('Windows', (3, 3), True):
         pass
