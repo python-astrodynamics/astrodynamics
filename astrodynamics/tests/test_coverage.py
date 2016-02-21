3db0a45c974eb66e56fc2899770882d5b88f8189
Frazer McLean <frazer@frazermclean.co.uk>
2016-02-21 03:27:02 +0100

    Don't need to print matched machine for test_coverage

--- astrodynamics/tests/test_coverage.py
+++ astrodynamics/tests/test_coverage.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 from __future__ import absolute_import, division, print_function
 
+import os
 import platform
 import sys
 
+import pytest
 from represent import ReprMixin
 
 
@@ -21,6 +23,8 @@ class Machine(ReprMixin, object):
                 self.x64 == other.x64)
 
 
+@pytest.mark.skipif(os.getenv('CI', '').lower() != 'true',
+                    reason="CI test.")
 def test_platform_coverage():
     machine = Machine(os=platform.system(),
                       python_version=sys.version_info[:2],
@@ -36,90 +40,73 @@ def test_platform_coverage():
 
     # The charade below is so that in our unified coverage sourced from Travis
     # and AppVeyor, we can see that all the combinations of Python version, OS,
-    # and 32/64 bits (Windows only) are tested.
+    # and 32 or 64 bits (Windows only) are tested.
 
     matches = 0
 
     testmachine = Machine(os='Darwin', python_version=(2, 7), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Darwin', python_version=(3, 3), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Darwin', python_version=(3, 4), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Darwin', python_version=(3, 5), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Linux', python_version=(2, 7), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Linux', python_version=(3, 3), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Linux', python_version=(3, 4), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Linux', python_version=(3, 5), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(2, 7), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(2, 7), x64=False)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(3, 3), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(3, 3), x64=False)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(3, 4), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(3, 4), x64=False)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(3, 5), x64=True)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
     testmachine = Machine(os='Windows', python_version=(3, 5), x64=False)
     if machine == testmachine:
-        print('\n> Machine matched {}'.format(testmachine))
         matches += 1
 
+    # If the test fails the machine that didn't match will be printed
     assert matches == 1, 'No match for {}'.format(machine)
-
-    print('\n> {}'.format(machine))
