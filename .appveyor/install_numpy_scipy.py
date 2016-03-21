835ee420e0f4fa7aa4100747593102a2e0b1465f
Frazer McLean <frazer@frazermclean.co.uk>
2016-03-21 22:29:33 +0100

    Install SciPy wheel too

--- .appveyor/install_numpy_scipy.py
+++ .appveyor/install_numpy_scipy.py
@@ -10,15 +10,14 @@ import sys
 from six.moves.urllib.request import urlretrieve
 
 
-def download_numpy_wheel():
-    base_url = os.getenv('NUMPY_URL')
+def download_wheel(name, version):
+    base_url = os.getenv('WHEEL_URL')
     if base_url is None:
-        raise ValueError('NUMPY_URL environment variable is missing.')
+        raise ValueError('WHEEL_URL environment variable is missing.')
 
-    version = '1.10.4+mkl'
     py = 'cp{0[0]}{0[1]}'.format(sys.version_info)
     if py not in {'cp27', 'cp34', 'cp35'}:
-        print('NumPy wheel not available for {}'.format(py))
+        print('{} wheel not available for {}'.format(name, py))
         return None
 
     bits = struct.calcsize('P') * 8
@@ -29,26 +28,38 @@ def download_numpy_wheel():
     else:
         raise ValueError("Couldn't determine 32/64 bits.")
 
-    filename = 'numpy-{}-{}-none-{}.whl'.format(version, py, arch)
+    filename = '{}-{}-{}-none-{}.whl'.format(name, version, py, arch)
 
-    directory = 'astrodynamics-numpy-wheels'
-    os.mkdir(directory)
+    directory = 'astrodynamics-wheels'
+    try:
+        os.mkdir(directory)
+    except OSError:
+        pass
 
     filepath = os.path.join(directory, filename)
     url = base_url + filename
 
-    # Disable SSL. Shouldn't do this ever. This is just a script.
-    ssl._create_default_https_context = ssl._create_unverified_context
     urlretrieve(url, filepath)
     return filepath
 
 
 def install_numpy():
-    filepath = download_numpy_wheel()
+    filepath = download_wheel('numpy', '1.10.4+mkl')
     if filepath:
         pip.main(['install', filepath])
     else:
         pip.main(['install', 'numpy'])
 
+
+def install_scipy():
+    filepath = download_wheel('scipy', '0.17.0')
+    if filepath:
+        pip.main(['install', filepath])
+    else:
+        pip.main(['install', 'scipy'])
+
 if __name__ == '__main__':
+    # Disable SSL. Shouldn't do this ever. This is just a script.
+    ssl._create_default_https_context = ssl._create_unverified_context
     install_numpy()
+    install_scipy()
