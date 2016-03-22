b876a133899195b8f2bb84df3daeb6433057066f
Frazer McLean <frazer@frazermclean.co.uk>
2016-03-22 23:42:43 +0100

    Update Python

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,12 +22,12 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
             pyenv global 3.3.6
             ;;
         py34)
-            pyenv install 3.4.3
-            pyenv global 3.4.3
+            pyenv install 3.4.4
+            pyenv global 3.4.4
             ;;
         py35)
-            pyenv install 3.5.0
-            pyenv global 3.5.0
+            pyenv install 3.5.1
+            pyenv global 3.5.1
             ;;
     esac
     pyenv rehash
