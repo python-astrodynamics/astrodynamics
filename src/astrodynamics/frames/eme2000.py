Fix frames for Rotation changes

src/astrodynamics/frames/eme2000.py
@@ -4,8 +4,6 @@
 # licenses/OREKIT.txt)
 from __future__ import absolute_import, division, print_function
 
-from math import sin
-
 import astropy.units as u
 import numpy as np
 from astropy._erfa import bi00
@@ -22,12 +20,18 @@ EME2000 = FrameProxy()
 @EME2000.register_factory
 def _():
     # Obliquity of the ecliptic.
-    EPSILON_0 = (84381.448 * u.arcsec).to(u.rad).value
+    EPSILON_0 = 84381.448 * u.arcsec
 
-    # D_PSI_B: Longitude correction
-    # D_EPSILON_B: Obliquity correction
-    # ALPHA_0: the ICRS right ascension of the J2000.0 mean equinox
     D_PSI_B, D_EPSILON_B, ALPHA_0 = bi00()
+
+    # Longitude correction
+    D_PSI_B *= u.rad
+
+    # Obliquity correction
+    D_EPSILON_B *= u.rad
+
+    # the ICRS right ascension of the J2000.0 mean equinox
+    ALPHA_0 *= u.rad
 
     J2000_EPOCH = Time('J2000', scale='tt')
 
@@ -37,7 +41,7 @@ def _():
 
     # Obliquity correction
     r1 = Rotation.from_axis_angle(axis=I, angle=D_EPSILON_B)
-    r2 = Rotation.from_axis_angle(axis=J, angle=-D_PSI_B * sin(EPSILON_0))
+    r2 = Rotation.from_axis_angle(axis=J, angle=-D_PSI_B * np.sin(EPSILON_0))
     r3 = Rotation.from_axis_angle(axis=K, angle=-ALPHA_0)
 
     transform_provider = FixedTransformProvider(

src/astrodynamics/frames/mod.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function
 
 from collections import namedtuple
 
+import astropy.units as u
 import numpy as np
 from astropy._erfa import p06e, obl06
 
@@ -38,14 +39,14 @@ class MODConventions2010TransformProvider(AbstractTransformProvider):
 
         I = np.array([1, 0, 0])
 
-        eps0 = obl06(date.jd1, date.jd2)
+        eps0 = obl06(date.jd1, date.jd2) * u.rad
         r4 = Rotation.from_axis_angle(I, eps0, convention='frame')
 
         results = P06eResults(*p06e(date.jd1, date.jd2))
 
-        psia = results.psia
-        oma = results.oma
-        chia = results.chia
+        psia = results.psia * u.rad
+        oma = results.oma * u.rad
+        chia = results.chia * u.rad
 
         precession = r4.compose(Rotation.from_euler_angles(
             RotationOrder.ZXZ, -psia, -oma, chia, convention='frame'))